89b86ad898458d21198f5d17ef1d1e12347a4f11
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-11 22:16:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 
 
-def using_push(colors_in_the_rainbow,string)
+def using_push(array,string)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   updated_array = using_push(colors_in_the_rainbow, next_color)
